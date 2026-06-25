9ca8988d1f1cf1d7f27adc9c85f85e6ed6848e14
tooru-cell <yamonetooru@gmail.com>
2026-06-25 16:02:43 +0900

    パスワード入力修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,11 +11,12 @@ class User < ApplicationRecord
             presence: true,
             uniqueness: true
 
-  validates :password,
-            length: { minimum: 3 },
-            if: -> { new_record? || changes[:crypted_password] }
+validates :password,
+          length: { minimum: 3 },
+          confirmation: true,
+          if: -> { new_record? || changes[:crypted_password] }
 
-  validates :password_confirmation,
-            presence: true,
-            if: -> { new_record? || changes[:crypted_password] }
+validates :password_confirmation,
+          presence: true,
+          if: -> { new_record? || changes[:crypted_password] }
 end
